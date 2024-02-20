57a86e4a9f8e78b872f573d56c9a7ca237da7f36
Viko025 <20030100@itesa.edu.mx>
2024-02-20 17:43:54 -0600

    Creación de los métodos Post, Get, Put y Delete en la API

---
+++ Sales.API/Controllers/CountriesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+using System.Runtime.CompilerServices;
+
+namespace Sales.API.Controllers
+
+{
+    [ApiController]
+    [Route("/api/countries")]
+
+
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public  CountriesController(DataContext context)
+        {
+           _context = context;
+        }
+        
+        //Método post
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country) 
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync() 
+        {
+            return Ok(await _context.Countries.ToListAsync());
+
+        }
+    }
+}
